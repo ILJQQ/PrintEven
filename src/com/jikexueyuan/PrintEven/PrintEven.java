aa6f2489def2e2bc990b2d25775d8501c394633a
ILJQQ <git@ILJQQ.com>
2017-02-06 17:35:53 +0800

    Android

---
+++ src/com/jikexueyuan/PrintEven/PrintEven.java
@@ -0,0 +1,9 @@
+package com.jikexueyuan.PrintEven;
+
+public class PrintEven {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 50; i++) {
+			System.out.println("零到一百所有的偶数是：" + 2 * i);
+		}
+	}
+}
